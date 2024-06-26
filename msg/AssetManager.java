de4c236b17743a00c2d99a48af9a204b1ef30044
GabrielHuttmann <gabrielhuttmann@outlook.com>
2024-06-26 17:59:09 -0300

    fix: created enum to replace switch on method manageMortage, removed unnecessary while loop and try/catch, removed unused vars

--- msg/AssetManager.java
+++ msg/AssetManager.java
@@ -104,10 +104,8 @@ public class AssetManager {
     //
     {
         try {
-            char option = '0';            // # of field to be changed
             boolean done = false;            // terminates while-loop
             boolean found = false;            // tells if mortgage is found
-            char c;                // character entered by user
             String input;                        // buffer for line of characters
             char choice;                    // user's choice
             Mortgage mortgage = new Mortgage();    // mortgage to be modified
@@ -135,73 +133,30 @@ public class AssetManager {
             }
 
             while (!done) {
-                while (!done) {
-                    UserInterface.clearScreen();
+                UserInterface.clearScreen();
 
-                    System.out.println("\t           UPDATE MORTGAGES\n\n");
-                    System.out.println("\t MARTHA STOCKTON GREENGAGE FOUNDATION\n\n");
-                    System.out.println("\t        1. Update mortgagee name\n");
-                    System.out.println("\t        2. Update price of home\n");
-                    System.out.println("\t        3. Update date mortgage issued\n");
-                    System.out.println("\t        4. Update current weekly income\n");
-                    System.out.println("\t        5. Update property tax\n");
-                    System.out.println("\t        6. Update insurance premium\n");
-                    System.out.println("\t        7. Update mortgage balance\n");
-                    System.out.println("\t        8. Exit to mortgage menu\n\n");
-                    System.out.println("Enter your choice and press <ENTER>: ");
+                System.out.println("\t           UPDATE MORTGAGES\n\n");
+                System.out.println("\t MARTHA STOCKTON GREENGAGE FOUNDATION\n\n");
+                System.out.println("\t        1. Update mortgagee name\n");
+                System.out.println("\t        2. Update price of home\n");
+                System.out.println("\t        3. Update date mortgage issued\n");
+                System.out.println("\t        4. Update current weekly income\n");
+                System.out.println("\t        5. Update property tax\n");
+                System.out.println("\t        6. Update insurance premium\n");
+                System.out.println("\t        7. Update mortgage balance\n");
+                System.out.println("\t        8. Exit to mortgage menu\n\n");
+                System.out.println("Enter your choice and press <ENTER>: ");
 
-                    try {
-                        choice = UserInterface.getChar();
+                choice = UserInterface.getChar();
 
-                        switch (choice) {
-                            case '1':
-                                mortgage.updateMortgageeName();
-                                break;
+                ManageMortgageOptions manageMortgageOptions = ManageMortgageOptions.fromChar(choice);
+                if (manageMortgageOptions != null) {
+                    done = manageMortgageOptions.execute(mortgage);
+                }
 
-                            case '2':
-                                mortgage.updatePrice();
-                                break;
-
-                            case '3':
-                                mortgage.updateDate();
-                                break;
-
-                            case '4':
-                                mortgage.updateWeeklyIncome();
-                                break;
-
-                            case '5':
-                                mortgage.updatePropertyTax();
-                                break;
-
-                            case '6':
-                                mortgage.updateInsurancePremium();
-                                break;
-
-                            case '7':
-                                mortgage.updateBalance();
-                                break;
-
-                            case '8':
-
-                            case '\n':
-                                done = true;
-                                break;
-
-                            default:
-                                System.out.println("\n\nNot a valid choice\n");
-                                UserInterface.pressEnter();
-                                break;
-                        }
-                    } catch (Exception e) {
-                        System.out.println("***** Error: AssetManager.manageMortgage () *****");
-                        System.out.println("\t" + e);
-                    }
-
-                    if (!done) {
-                        mortgage.print();
-                        UserInterface.pressEnter();
-                    }
+                if (!done) {
+                    mortgage.print();
+                    UserInterface.pressEnter();
                 }
             }
 
